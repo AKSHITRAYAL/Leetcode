858e8e3f90ac6d343293f538b30711df8b5e6844
AKSHITRAYAL <137927348+AKSHITRAYAL@users.noreply.github.com>
2026-02-28 02:22:16 +0530

    Time: 0 ms (100%), Space: 44.7 MB (29.31%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i=0;i<operations.length;i++){
             if(operations[i].charAt(1)=='+'){
             // if(operations[i].contains("+")){
+            // this checks like a searching inside the loop of the string array at every position with a brute force method 
                 x++;
             }
             else if(operations[i].charAt(1)=='-'){
