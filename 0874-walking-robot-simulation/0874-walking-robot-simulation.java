fc2f1a54ecca1d107e4800b87802419798aaafc6
AKSHITRAYAL <137927348+AKSHITRAYAL@users.noreply.github.com>
2026-04-06 23:46:48 +0530

    Time: 12 ms (97.83%), Space: 50.4 MB (85.87%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.java
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.java
@@ -1,43 +1,52 @@
+import java.util.*;
+
 class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
 
-        Set<String> obstacleSet = new HashSet<>();
-        for (int[] obstacle: obstacles) {
-            obstacleSet.add(obstacle[0] + "," + obstacle[1]);
+        Set<Long> obstacleSet = new HashSet<>();
+
+        // Encode (x, y) into a single long
+        for (int[] o : obstacles) {
+            long key = (((long)o[0]) << 32) | (o[1] & 0xffffffffL);
+            obstacleSet.add(key);
         }
-        
-        // North - 0, East - 1, South - 2, West - 3
+
         int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
         int dirIdx = 0;
+
         int x = 0, y = 0;
         int maxDistSqr = 0;
 
-        for (int command: commands) {
+        for (int command : commands) {
 
             if (command == -1) {
                 dirIdx = (dirIdx + 1) % 4;
             } else if (command == -2) {
                 dirIdx = (dirIdx + 3) % 4;
             } else {
+
                 int dx = directions[dirIdx][0];
                 int dy = directions[dirIdx][1];
 
-                int step = 0;
-                while (step < command) {
+                for (int step = 0; step < command; step++) {
+
                     int nextX = x + dx;
                     int nextY = y + dy;
 
-                    if (obstacleSet.contains(nextX + "," + nextY)) {
+                    long key = (((long)nextX) << 32) | (nextY & 0xffffffffL);
+
+                    if (obstacleSet.contains(key)) {
                         break;
                     }
+
                     x = nextX;
                     y = nextY;
 
                     maxDistSqr = Math.max(maxDistSqr, x*x + y*y);
-                    step++;
                 }
             }
         }
+
         return maxDistSqr;
     }
 }
