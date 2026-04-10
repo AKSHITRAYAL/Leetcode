fb2dfe592a47b11f69c7e5b86015e3dca1ac69e1
AKSHITRAYAL <137927348+AKSHITRAYAL@users.noreply.github.com>
2026-04-10 23:31:24 +0530

    Time: 23 ms (91.93%), Space: 69.2 MB (64.04%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,19 +1,25 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-
-        for (int num : nums) {
-            int complement = k - num;
-
-            if (map.getOrDefault(complement, 0) > 0) {
+        Arrays.sort(nums);
+        
+        int left=0,right=nums.length-1;
+        int count=0;
+        while(left<right){
+            int sum=nums[left]+nums[right];
+            
+            if(sum==k){
+                left++;
+                right--;
                 count++;
-                map.put(complement, map.get(complement) - 1);
-            } else {
-                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+            else if(sum>k){
+                right--;
+            }
+            else{
+                left++;
             }
         }
-
+        
         return count;
     }
 }
